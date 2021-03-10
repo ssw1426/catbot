3c99b9c7ea57d2bcaeec21e7e78758d47750b1bd
ssw1426 <80308398+ssw1426@users.noreply.github.com>
2021-03-10 11:56:08 +0900

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,10 +1,11 @@
 import discord
+import os
 
 client = discord.Client()
-token = "ODE4NzAzMjI1NDU2Njg5MTgz.YEb6yw.KRKJduNxaZDX_psSrk8NhI8Y7ZE"
 
 @client.event
 async def on_ready():
     print("봇이 온라인으로 전환되었습니다.")
 
-client.run(token)
+access_token = os.environ["BOT_TOKEN"]
+client.run(access_token)
